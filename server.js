f63f8f3385ac39be1416a9c4e0a2478c833a7b1a
darshakshah1988 <darshakshah1988@gmail.com>
2024-03-26 23:12:13 +0530

    new updates and razorpay routes and new databases

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
+const Razorpay = require('razorpay');
+
+// Razorpay setup
+const razorpay = new Razorpay({
+  key_id: process.env.RAZOR_PAY_KEY,
+  key_secret: process.env.RAZOR_PAY_SECRET
+});
 
 // MongoDB connection
 mongoose.connect('mongodb+srv://smartdarshak88:Darshak%401988@ganpatiwalacluster.yp8m6kk.mongodb.net', {
@@ -29,6 +37,10 @@ const User = mongoose.model('User', {
   password: String,
   subscriptionPlan: { type: mongoose.Schema.Types.ObjectId, ref: 'SubscriptionPlan' },
   active: Boolean,
+  agent: Boolean,
+  razorpayPlan: String,
+  email: String,
+  contact: String
 });
 
 const Product = mongoose.model('Product', {
@@ -52,9 +64,54 @@ const ProductQuery = mongoose.model('ProductQuery', {
   updatedAt: { type: Date, default: Date.now },
 });
 
+const Orders = mongoose.model('Orders', {
+  orderId: String,
+  customerId: String,
+  userId: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+})
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+//check if razorpay customer is exists or not
+// Function to check if a customer exists
+async function checkCustomerExists(name,email,contact) {
+  try {
+    // Fetch the customer from Razorpay based on email
+    const customer = await razorpay.customers.create({
+      name: name,
+      email: email,
+      contact: contact // Provide customer's contact number
+    });
+
+    return customer.id;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// create orders in razorpay 
+async function createOrder(amount, currency, receipt, customerId) {
+  try {
+    // Create the order in Razorpay
+    const order = await razorpay.orders.create({
+      amount: amount * 100, // Razorpay expects amount in smallest currency unit (paise)
+      currency: currency,
+      receipt: receipt,
+      customer_id: customerId,
+      payment_capture: 0
+    });
+
+    return order;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
 
 // Routes
 
@@ -73,15 +130,118 @@ app.post('/subscription-plan', async (req, res) => {
 // 2) Register User with subscription plan selection
 app.post('/register', async (req, res) => {
   try {
-    const { username, password, subscriptionPlanId, active=0 } = req.body;
+    const { username, password, subscriptionPlanId, active = 0, agent = false, razorpayPlan = "", email, contact} = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword, subscriptionPlan: subscriptionPlanId, active });
+    const user = new User({ username, password: hashedPassword, subscriptionPlan: subscriptionPlanId, active, agent, razorpayPlan });
     await user.save();
 
     res.json({ success: true, user });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.post('/vregister', async (req, res) => {
+  try {
+    const { username, password, subscriptionPlanId, active = 0, agent = true, email, contact } = req.body;
+    var razorpayPlan = "";
+    var razorpayPrice = 0;
+    var customerId;
+    
+
+    if(subscriptionPlanId == "65ac3edc9e8cc51805bfcd9e")
+    {
+      razorpayPlan = process.env.BASIC_PLAN
+      razorpayPrice = process.env.BASIC_PLAN_PRICE
+    }
+    else if(subscriptionPlanId == "65ac3f1e9e8cc51805bfcd9f")
+    {
+      razorpayPlan = process.env.PRO_PLAN
+      razorpayPrice = process.env.PRO_PLAN_PRICE
+    }
+    else if(subscriptionPlanId == "65ac3f2f9e8cc51805bfcda0")
+    {
+      razorpayPlan = process.env.BUSINESS_PLAN
+      razorpayPrice = process.env.BUSINESS_PLAN_PRICE
+    }
+    else
+    {
+
+    }
+    
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword, subscriptionPlan: subscriptionPlanId, active, agent, razorpayPlan });
+    await user.save();
+
+    const customer = await razorpay.customers.all({query: {email: email}});
+    const result = customer.items.filter((e) => {
+      return e.email === email;
+    })
+
+    if(result.length > 0)
+    {
+      customerIsExists = 1;
+      customerId = result[0].id;
+      const order = await createOrder(razorpayPrice, "INR", "OnlineOrder", customerId);
+      console.log(order);
+      const orderId = order.id;
+      const userId = user._id;
+      const successOrder = new Orders({orderId, customerId, userId});
+      await successOrder.save();
+    }
+    else
+    {
+      customerIsExists = 0;
+      customerId = await checkCustomerExists(username, email, contact);
+      console.log(customerId);
+      const order = await createOrder(razorpayPrice, "INR", "OnlineOrder", customerId);
+      console.log(order);
+      const orderId = order.id;
+      const userId = user._id;
+      const successOrder = new Orders({orderId, customerId, userId});
+      await successOrder.save();
+    }
+
+    
+    
+    
+
+    // Define subscription parameters
+    // const subscriptionOptions = {
+    //   plan_id: razorpayPlan, // The ID of the plan created in your Razorpay dashboard
+    //   customer_id: customerId, // The ID of the customer for whom you are creating the subscription
+    //   total_count: 1, // Total billing cycles, adjust as needed
+    //   quantity: 1, // Number of subscriptions, adjust as needed
+    //   notes: {
+    //     // Optional notes
+    //     description: 'One time subscription',
+    //     frequency: 'Yearly'
+    //   }
+    // };
+
+    // Create subscription
+    // await razorpay.subscriptions.create(subscriptionOptions, (error, subscription) => {
+    //   if (error) {
+    //     console.error('Subscription creation failed:', error);
+    //   } else {
+    //     console.log('Subscription created successfully:', subscription);
+    //   }
+    // });
+
+    res.json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+//2.5 check for webhook notifications from razorpay
+app.get("/payment/callback", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch(error) {
+    console.log(error);
   }
 });
 
@@ -120,9 +280,44 @@ app.post('/login', async (req, res) => {
   }
 });
 
+app.post('/vlogin', async (req, res) => {
+  try {
+    const { username, password, agent } = req.body;
+
+    const user = await User.findOne({ username, agent: true }).populate('subscriptionPlan');
+    console.log("user",user);
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'Authentication failed. Agent not found.' });
+    }
+
+    if (user.active !== true) {
+      return res.status(401).json({ success: false, message: 'Authentication failed. Agent status not active.' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ success: false, message: 'Authentication failed. Incorrect password.' });
+    }
+
+    const data = user;
+
+    // Generate JWT token with user data and subscription plan details
+    const token = jwt.sign(
+      { _id: user._id, username: user.username, subscriptionPlan: user.subscriptionPlan },
+      'secret_key'
+    );
+
+    res.json({ success: true, token, data });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
 app.get('/products', (req, res) => {
   try {
-    const { limit,vendor,tags,order,random } = req.query;
+    const { limit, vendor, tags, order, random } = req.query;
 
     // Read products from the JSON file
     const productsData = fs.readFileSync('products.json');
@@ -133,9 +328,9 @@ app.get('/products', (req, res) => {
     // tags filter if exists
     const filteredProducts = tags
       ? activeProducts.filter(product => {
-          const productTags = product.tags || [];
-          return tags.split(',').every(tag => productTags.includes(tag.trim()));
-        })
+        const productTags = product.tags || [];
+        return tags.split(',').every(tag => productTags.includes(tag.trim()));
+      })
       : activeProducts;
     // Applied Vendor filter
     const vendorProducts = vendor ? filteredProducts.filter(product => product.vendor === vendor) : activeProducts;
@@ -173,7 +368,7 @@ app.get('/subscription-plans', async (req, res) => {
 
 app.get('/decorations', (req, res) => {
   try {
-    const { limit,vendor,tags,order,random } = req.query;
+    const { limit, vendor, tags, order, random } = req.query;
 
     // Read products from the JSON file
     const productsData = fs.readFileSync('decorations.json');
@@ -184,9 +379,9 @@ app.get('/decorations', (req, res) => {
     // // tags filter if exists
     const filteredProducts = tags
       ? activeProducts.filter(product => {
-          const productTags = product.tags || [];
-          return tags.split(',').every(tag => productTags.includes(tag.trim()));
-        })
+        const productTags = product.tags || [];
+        return tags.split(',').every(tag => productTags.includes(tag.trim()));
+      })
       : activeProducts;
     // Applied Vendor filter
     const vendorProducts = vendor ? filteredProducts.filter(product => product.vendor === vendor) : activeProducts;
@@ -249,7 +444,7 @@ app.post('/products', async (req, res) => {
 
     // Find the subscription plan to check the limit
     const subscriptionPlan = await SubscriptionPlan.findById(subscriptionPlanId);
-    
+
     if (!subscriptionPlan) {
       return res.status(404).json({ success: false, message: 'Subscription plan not found.' });
     }
@@ -283,7 +478,7 @@ app.post('/products', async (req, res) => {
   }
 });
 
-app.post("/idolsQuery", async (req,res) => {
+app.post("/idolsQuery", async (req, res) => {
   try {
     const { name, productName, message } = req.body;
 
