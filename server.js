97a0ce90ddff966db4d686cac91c84928117f58c
darshakshah1988 <darshakshah1988@gmail.com>
2024-05-07 18:01:24 +0530

    check on send OTP

--- server.js
+++ server.js
@@ -194,14 +194,15 @@ const sendOTP = async (mobileNumber) => {
 app.post("/verify-mobile", async(req,res) => {
   const mobileNumber = req.body.mobile;  // Replace with the mobile number
   const verify = sendOTP(mobileNumber);
-  if(verify == 0)
-  {
-    res.status(401).json({status: "failed", message: "could not send the OTP" });
-  }
-  else
-  {
-  res.status(200).json({status: "success", message: "OTP sent"});
-  }
+  console.log(verify);  
+  // if(verify == 0)
+  // {
+  //   res.status(401).json({status: "failed", message: "could not send the OTP" });
+  // }
+  // else
+  // {
+  // res.status(200).json({status: "success", message: "OTP sent"});
+  // }
 })
 
 app.post('/vregister', async (req, res) => {
